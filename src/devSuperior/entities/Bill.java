eecd8387c01e848960dc46c7745aa72cf7677f7e
alcid <alcides_thiago@hotmail.com>
2022-10-03 14:50:34 -0300

    Update Project

--- src/devSuperior/entities/Bill.java
+++ src/devSuperior/entities/Bill.java
@@ -2,18 +2,18 @@ package devSuperior.entities;
 
 public class Bill {
 
-    public  final double COUVER = 4.0;
+    public final double COUVER = 4.0;
 
-    public  char gender;
-    public  int beer;
-    public  int barbecue;
-    public  int softDrink;
+    public char gender;
+    public int beer;
+    public int barbecue;
+    public int softDrink;
 
-    public  double cover(){
+    public double cover(){
         return COUVER;
     }
 
-    public  double feeding(){
+    public double feeding(){
         double v = beer * 5.00 + barbecue * 7.00 + softDrink * 3.00;
         return v;
     }
